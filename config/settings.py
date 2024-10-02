2c2a89bc91e3177b66be067edb952ad384d964ea
l-yoon <min>
2024-10-02 17:17:50 +0900

    cd

--- config/settings.py
+++ config/settings.py
@@ -29,10 +29,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-print(SECRET_KEY)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
-print(DEBUG)
+
 ALLOWED_HOSTS = ['43.203.226.5', '127.0.0.1', 'localhost',]
 
 
